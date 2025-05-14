a498ab8aed48e65359c281a9280320d2f21109f0
denev <denev723@gmail.com>
2025-05-14 20:18:59 +0900

    package.json 및 package-lock.json에 javascript-obfuscator 추가, main.js 파일에 JS 난독화 및 축소화 스크립트 추가, footer.scss에 가족 메뉴 활성화 스타일 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,29 @@
-function calcSideLeft() {
-  const $siteSide = $(".site-side");
-  const $siteWrapLeft = $("#wrap").offset().left;
-  if ($siteWrapLeft > 0) {
-    $siteSide.css("left", $siteWrapLeft);
+$(document).ready(function () {
+  function calcSideLeft() {
+    const $siteSide = $(".site-side");
+    const $siteWrapLeft = $("#wrap").offset().left;
+    if ($siteWrapLeft > 0) {
+      $siteSide.css("left", $siteWrapLeft);
+    }
   }
-}
 
-calcSideLeft();
-$(window).on("resize", calcSideLeft);
+  calcSideLeft();
+  $(window).on("resize", calcSideLeft);
+
+  $(".btn-family").on("click", function (e) {
+    e.stopPropagation();
+    $(".site-footer__family").toggleClass("site-footer__family--active");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$(e.target).closest(".site-footer__family, .btn-family").length) {
+      $(".site-footer__family").removeClass("site-footer__family--active");
+    }
+  });
+
+  $(document).on("keydown", function (e) {
+    if (e.key === "Escape") {
+      $(".site-footer__family").removeClass("site-footer__family--active");
+    }
+  });
+});
